b8fcbcac9aa64c3c8f03e3335e7f326322be15da
storvik <petterstorvik@gmail.com>
2017-11-28 21:41:22 +0100

    Make config cross platform

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # Emacs configurations
+
+Cross platform emacs configuration.
+Should work in Linux/Mac OS X/Windows, but some tools and packages may only be available in Linux/Mac.
+
+## Files
+
+- `init.el` is the starting point when opening Emacs
+- `emacs_init.org` contains all common configs which works on all platforms
+- `emacs_<platform>.org` contains platform specific configs
+- `emacs_unix.org` contains configs which works in Linux and Mac OS X only
--- init.el
+++ init.el
@@ -1,10 +1,12 @@
 (package-initialize)
 
-;; Keep emacs Custom-settings in separate file
+;; Keep emacs Custom-settings in separate file, load if exists
 (setq custom-file (expand-file-name "custom.el" user-emacs-directory))
-(load custom-file)
+(when (file-exists-p custom-file)
+  (load custom-file))
 
+;; Require org and load emacs_init.org
 (require 'org)
-(org-babel-load-file "~/.emacs.d/emacs.org")
+(org-babel-load-file "~/.emacs.d/emacs_init.org")
 (put 'upcase-region 'disabled nil)
 (put 'downcase-region 'disabled nil)
