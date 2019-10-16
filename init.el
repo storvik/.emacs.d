4df65b6def1d7f121d35ff4759d106e6e8ffeb15
storvik <petterstorvik@gmail.com>
2019-10-16 23:01:33 +0200

    Fix org mode initialization

--- init.el
+++ init.el
@@ -1,10 +1,31 @@
+;; Bootstrap straight.el
+(defvar bootstrap-version)
+(let ((bootstrap-file
+       (expand-file-name "straight/repos/straight.el/bootstrap.el" user-emacs-directory))
+      (bootstrap-version 5))
+  (unless (file-exists-p bootstrap-file)
+    (with-current-buffer
+        (url-retrieve-synchronously
+         "https://raw.githubusercontent.com/raxod502/straight.el/develop/install.el"
+         'silent 'inhibit-cookies)
+      (goto-char (point-max))
+      (eval-print-last-sexp)))
+  (load bootstrap-file nil 'nomessage))
+
+;; Make use-package use straight and ensure packages are installed
+(straight-use-package 'use-package)
+(setq straight-use-package-by-default t)
+
 ;; Keep emacs Custom-settings in separate file, load if exists
 (setq custom-file (expand-file-name "custom.el" user-emacs-directory))
 (when (file-exists-p custom-file)
   (load custom-file))
 
+;; Install org before using it to load init file
+(use-package org-plus-contrib
+  :mode ("\\.org$" . org-mode))
+
 ;; Require org and load emacs_init.org
-(require 'org)
 (org-babel-load-file (expand-file-name "emacs_init.org" user-emacs-directory))
 (put 'upcase-region 'disabled nil)
 (put 'downcase-region 'disabled nil)
