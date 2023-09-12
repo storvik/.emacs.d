8873de6aaf028b40c62c58d99146e052b6dc3d39
storvik <petterstorvik@gmail.com>
2023-09-12 09:19:38 +0200

    developer: add lsp-nix

--- stormacs-modules/init-developer-nix.el
+++ stormacs-modules/init-developer-nix.el
@@ -1,8 +1,16 @@
 ;; init-developer-nix.el --- Developer nix settings -*- lexical-binding: t; -*-
 
+(use-package lsp-nix
+  :elpaca (nix-mode :host github :repo "NixOS/nix-mode")
+  :after (lsp-mode)
+  :demand t
+  :custom
+  (lsp-nix-nil-formatter ["nixpkgs-fmt"]))
+
 (use-package nix-mode
   :elpaca (nix-mode :host github :repo "NixOS/nix-mode")
-  :mode "\\.nix\\'")
+  :mode "\\.nix\\'"
+  :hook (nix-mode . lsp-deferred))
 
 (use-package nix-update
   :elpaca (nix-update :host github :repo "jwiegley/nix-update-el"))
