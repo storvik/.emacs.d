195dc9be8b39fb159045fe3951da273102d5bc95
storvik <petterstorvik@gmail.com>
2024-01-03 09:24:47 +0100

    developer: add flutter.el

--- stormacs-modules/init-developer-dart.el
+++ stormacs-modules/init-developer-dart.el
@@ -3,4 +3,7 @@
 (use-package dart-mode
   :elpaca (dart-mode :host github :repo "bradyt/dart-mode"))
 
+(use-package flutter
+  :elpaca (flutter :host github :repo "amake/flutter.el"))
+
 (provide 'init-developer-dart)
