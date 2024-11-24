9ffa4250fe6466b2c562d5e42d7e4863ce0a6477
storvik <petterstorvik@gmail.com>
2024-11-24 22:16:57 +0100

    dired: add dired-sidebar

--- stormacs-modules/init-dired.el
+++ stormacs-modules/init-dired.el
@@ -34,4 +34,20 @@
   (add-to-list 'dired-preview-trigger-commands #'diredp-previous-line)
   (dired-preview-global-mode 1))
 
+(use-package dired-sidebar
+  :ensure (dired-sidebar :host github :repo "jojojames/dired-sidebar")
+  :commands (dired-sidebar-toggle-sidebar)
+  :bind (("C-x C-n" . dired-sidebar-toggle-sidebar))
+  :init
+  (add-hook 'dired-sidebar-mode-hook
+            (lambda ()
+              (unless (file-remote-p default-directory)
+                (auto-revert-mode))))
+  :config
+  (push 'toggle-window-split dired-sidebar-toggle-hidden-commands)
+  (push 'rotate-windows dired-sidebar-toggle-hidden-commands)
+
+  (setq dired-sidebar-theme 'nerd)
+  (setq dired-sidebar-use-custom-font t))
+
 (provide 'init-dired)
