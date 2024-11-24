224ba89c89a569bc5ccb1a3185ab4c9d6a486ba3
storvik <petterstorvik@gmail.com>
2024-11-24 22:17:56 +0100

    developer: remove codeium and default clang formatter rules

--- stormacs-modules/init-developer.el
+++ stormacs-modules/init-developer.el
@@ -66,8 +66,6 @@
 (use-package apheleia
   :ensure (apheleia :host github :repo "raxod502/apheleia")
   :config
-  (setf (alist-get 'clang-format apheleia-formatters)
-        '("clang-format" "-style={BasedOnStyle: Google, IndentWidth: 4, ColumnLimit: 120, SortIncludes: false}"))
   (add-to-list 'apheleia-formatters '(goimports "goimports"))
   (setf (alist-get 'go-mode apheleia-mode-alist) 'goimports)
   (setf (alist-get 'go-ts-mode apheleia-mode-alist) 'goimports)
@@ -76,29 +74,6 @@
   (setf (alist-get 'python-ts-mode apheleia-mode-alist) 'ruff)
   (setf (alist-get 'python-mode apheleia-mode-alist) 'ruff)
   (apheleia-global-mode +1))
-
-(use-package codeium
-  :ensure (codeium :host github :repo "Exafunction/codeium.el")
-  :custom
-  ;; Use codeium from system path
-  (codeium-command-executable "codeium_language_server")
-  :init
-  ;; Enable codeium globally, can also be enabled per mode
-  (add-to-list 'completion-at-point-functions #'codeium-completion-at-point)
-  :config
-  ;; use M-x codeium-diagnose to see apis/fields that would be sent to the local language server
-  (setq codeium-api-enabled
-        (lambda (api)
-          (memq api '(GetCompletions Heartbeat CancelRequest GetAuthToken RegisterUser auth-redirect AcceptCompletion))))
-  ;; Limit the string sent to codeium for better performance
-  (defun my-codeium/document/text ()
-    (buffer-substring-no-properties (max (- (point) 3000) (point-min)) (min (+ (point) 1000) (point-max))))
-  ;; If text is changed, cursor_offset shoult also be changed
-  (defun my-codeium/document/cursor_offset ()
-    (codeium-utf8-byte-length
-     (buffer-substring-no-properties (max (- (point) 3000) (point-min)) (point))))
-  (setq codeium/document/text 'my-codeium/document/text)
-  (setq codeium/document/cursor_offset 'my-codeium/document/cursor_offset))
 
 (require 'init-developer-c)
 (require 'init-developer-config)
